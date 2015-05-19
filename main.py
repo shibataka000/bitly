8b62f37a7fedaadf5ab5e2fcbaf906c716bd803d
Takao Shibata <takao.shibata@gmail.com>
2015-05-20 06:12:54 +0900

    メソッドgを展開した

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import BaseHTTPServer as B,cgi,hashlib as h,random as r
 d,p={"":""},8080
-def g(f):
-    s=""
-    while s in d:
-        s=h.md5(f+str(r.random())).hexdigest()[:9]
-    d["/"+s]=f
-    return s
 class I(B.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path in d:
@@ -15,8 +9,11 @@ class I(B.BaseHTTPRequestHandler):
         else:
             self.send_error(500)
     def do_POST(self):
-        f=cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={"REQUEST_METHOD":"POST"}).file.read()
+        s,f="",cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={"REQUEST_METHOD":"POST"}).file.read()
+        while s in d:
+            s=h.md5(f+str(r.random())).hexdigest()[:9]
+        d["/"+s]=f
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("http://localhost:"+str(p)+"/"+g(f))
+        self.wfile.write("http://localhost:"+str(p)+"/"+s)
 B.HTTPServer(("",p),I).serve_forever()
