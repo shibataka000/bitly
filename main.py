b50e7e813634b7181c9d417782816be74ca914cb
Takao Shibata <takao.shibata@gmail.com>
2015-05-20 22:09:12 +0900

    URLをパラメータとして分離した

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import BaseHTTPServer as B,cgi,hashlib as h,random as r
-d,p={"":""},8080
+u,p,d="http://localhost",8080,{"":""}
 class I(B.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path in d:
@@ -15,5 +15,5 @@ class I(B.BaseHTTPRequestHandler):
         d["/"+s]=f
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("http://localhost:"+str(p)+"/"+s)
+        self.wfile.write(u+":"+str(p)+"/"+s)
 B.HTTPServer(("",p),I).serve_forever()
