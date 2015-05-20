72465b23798ac0059b276962f403d05ee6d47ce7
Takao Shibata <takao.shibata@gmail.com>
2015-05-20 22:35:27 +0900

    ローカルのredis-serverに情報を格納するよう修正

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import BaseHTTPServer as B,cgi,hashlib as h,random as r
-u,p,d="http://localhost",8080,{"":""}
+import BaseHTTPServer as B,cgi,hashlib as h,random as r,redis as e
+u,p="localhost",8080
+d=e.StrictRedis(host=u)
+d.set("","")
 class I(B.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path in d:
+        if d.exists(self.path):
             self.send_response(301)
-            self.send_header("Location",d[self.path])
+            self.send_header("Location",d.get(self.path))
             self.end_headers()
         else:
             self.send_error(500)
     def do_POST(self):
         s,f="",cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={"REQUEST_METHOD":"POST"}).file.read()
-        while s in d:
+        while d.exists(s):
             s=h.md5(f+str(r.random())).hexdigest()[:9]
-        d["/"+s]=f
+        d.set("/"+s,f)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(u+":"+str(p)+"/"+s)
+        print s
+        self.wfile.write("http://%s:%d/%s"%(u,p,s))
 B.HTTPServer(("",p),I).serve_forever()
